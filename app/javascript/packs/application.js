a18d865c021c541ebd1086e2849128ff87794c51
zumbam <stefanronczka@outlook.de>
2020-02-14 16:08:18 +0100

    Adding staging ENV for production similar local build also add vendor folder into assets manifest

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,7 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-import("semantic")
+import "semantic"
 import scroll_bottom from "./utils"
 
 
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,8 +6,17 @@ Rails.application.config.assets.version = '1.0'
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
 # Add Yarn node_modules folder to the asset load path.
+# Rails.application.config.assets.paths << Rails.root.join('vendor')
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
 
+# Rails.application.config.assets.paths << Rails.root.join('vendor/assets/fomantic-ui/themes/basic/assets/fonts')
+# Rails.application.config.assets.paths += Dir["#{Rails.root}/lib/assets/*"]
+# Rails.application.config.assets.precompile = ["vendor/config/manifest.js"]
+# Rails.application.config.assets.precompile << Proc.new { |path|
+#   if path =~ /\.(eot|svg|ttf|woff)\z/
+#     true
+#   end
+# }
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
